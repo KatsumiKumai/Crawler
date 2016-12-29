5deec2e6fc35a364b263e86bd0a0d18f77635e69
KatsumiKumai <katsumi@KatsumiKumainoMacBook-Pro.local>
2016-12-30 01:13:42 +0900

    フォルダ整理

--- chapter2/scraping.rb
+++ chapter2/scraping.rb
@@ -12,13 +12,14 @@ urls.push("http://amazon.co.jp/gp/bestsellers/books/466282/")
 Anemone.crawl(urls,:depth_limit => 0) do |anemone|
   anemone.on_every_page do |page|
     #文字コードをUTF8に変換した上で、Nokogiriでパース
-    doc = Nokogiri::HTML.parse(page.body.toutf8)
+    #doc = Nokogiri::HTML.parse(page.body.toutf8)
 
-    category = doc.xpath("//*[@id='zg_browseRoot']/ul/li/a").text
+    #category = doc.xpath("//*[@id='zg_browseRoot']/ul/li/a").text
 
     #カテゴリ名の表示
-    sub_category = doc.xpath("//*[@id=\"zg_listTitle\"]/span").text
+    #sub_category = doc.xpath("//*[@id=\"zg_listTitle\"]/span").text
 
-    puts category+"/"+sub_category
+    #puts category+"/"+sub_category
+    puts page.url
   end
 end
